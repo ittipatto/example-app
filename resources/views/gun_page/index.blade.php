4bd08d9e3785f6c862848eaa33841b0d2188e70f
ittipatto <ittipat.to@kkumail.com>
2022-06-14 11:13:04 +0700

    'master'

--- resources/views/gun_page/index.blade.php
+++ resources/views/gun_page/index.blade.php
@@ -4,14 +4,14 @@
 
 @section('content')
     <h1>Gun</h1>
-    <div class="mb-2">
+    {{-- <div class="mb-2">
         <a href="{{ url('content/create') }}" role="button" class="btn btn-sm btn-success">Create</a>
 
-    </div>
+    </div> --}}
     <table id="tbContent" class="table table-border">
         <thead>
             <tr>
-                <th>id</th>
+                {{-- <th>id</th> --}}
                 <th>name</th>
                 <th>image</th>
                 <th>detail</th>
