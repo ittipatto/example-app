f1e19cd7814837a22777d9ad1b11ab372d5107f4
ittipatto <ittipat.to@kkumail.com>
2022-06-14 09:50:43 +0700

    'master'

--- database/migrations/9022_05_12_104428_create_relations_table.php
+++ database/migrations/9022_05_12_104428_create_relations_table.php
@@ -13,30 +13,6 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('use_ones', function (Blueprint $table) {
-            $table->foreign('storage_id')->references('id')->on('storage_images');
-            $table->foreign('one_id')->references('id')->on('ones');
-        });
-
-        Schema::table('use_twos', function (Blueprint $table) {
-            $table->foreign('storage_id')->references('id')->on('storage_images');
-            $table->foreign('two_id')->references('id')->on('twos');
-        });
-
-        Schema::table('use_threes', function (Blueprint $table) {
-            $table->foreign('storage_id')->references('id')->on('storage_images');
-            $table->foreign('three_id')->references('id')->on('threes');
-        });
-
-        Schema::table('use_fours', function (Blueprint $table) {
-            $table->foreign('storage_id')->references('id')->on('storage_images');
-            $table->foreign('four_id')->references('id')->on('fours');
-        });
-
-        Schema::table('use_fives', function (Blueprint $table) {
-            $table->foreign('storage_id')->references('id')->on('storage_images');
-            $table->foreign('five_id')->references('id')->on('fives');
-        });
     }
 
     /**
